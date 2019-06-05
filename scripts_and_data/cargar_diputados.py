605fd377e774d80468e5d833befe5fb9f9fc060b
d3ah <diegoeauyonh@gmail.com>
2019-06-05 15:00:30 -0600

    Actualizando script para crear candidatos.

--- scripts_and_data/cargar_diputados.py
+++ scripts_and_data/cargar_diputados.py
@@ -1,32 +1,40 @@
-import csv
+# coding=utf-8
+import unicodecsv as csv
+import io
+import codecs
 
 from elections.models import Candidate, Election, PersonalData, Area
 
 from backend_candidate.models import CandidacyContact
-from backend_candidate.models import send_mail_with_user_and_password
 from backend_candidate.send_mails_to_candidates import send_user_to_candidates
 
 from votai_utils.send_mails import validateEmail
 
-reader = csv.DictReader(open('scripts_and_data/8may.csv'))
+reader = codecs.open('scripts_and_data/data/nuestraeleccion2.csv', 'r', encoding='utf-8')
 
-counter = 0
+# Seleccionamos la elección a la cual pertenece este candidato
+election = Election.objects.get(name="Diputado por Listado Nacional")
 
 for info_diputado in reader:
-    diputado, isCreated = Candidate.objects.get_or_create(name=unicode(info_diputado['Nombre completo'], 'utf-8'))
-    # print(diputado)
-    partido = info_diputado['Partido']
-    # print(partido)
+    info_diputado = info_diputado.split(u',')
+    # Creamos candidato, o lo obtenemos por su nombre (posible conflicto con candidatos con el mismo nombre),
+    # preferiblemente ser cuidadosos y utilizar "create".
+    candidate, created = Candidate.objects.get_or_create(name=info_diputado[1])
+    if created:
+        election.candidates.add(candidate)
+    # Verificamos si tiene partido, si es el caso, le asignamos ese partido
+    partido = info_diputado[0]
     if partido:
-        PersonalData.objects.get_or_create(candidate=diputado, label=u'Partido', value=partido)
-    
-    email = unicode(info_diputado['CORREO ELECTRÓNICO'], 'utf-8')
-    if email and validateEmail(email):
-        contacto, isCreated = CandidacyContact.objects.get_or_create(candidate=diputado, mail=email)
-        # if not isCreated:
-        #     contacto.send_mail_with_user_and_password()
-    
-    counter += 1
-    # send_candidate_username_and_password(diputado)
-    if not counter % 5:
-        print(counter)
+        PersonalData.objects.get_or_create(candidate=candidate, label=u'Partido', value=partido)
+    # Verificamos si tiene correo electrónico, si es el caso, le asignamos ese correo.
+    email = info_diputado[5]
+    print(email)
+    if email:
+        print('Enviar correo?')
+        contacto, isCreated = CandidacyContact.objects.get_or_create(candidate=candidate, mail=email)
+        # Si es nuevo contacto, nueva llave entre candidato y correo. * Pueden haber correos iguales para diferentes candidatos.
+        if isCreated:
+            send_candidate_username_and_password(candidate)
+
+# [a.delete() for a in Candidate.objects.all()]
+# [a.delete() for a in CandidacyContact.objects.all()]
